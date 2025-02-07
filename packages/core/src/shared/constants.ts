5193e496b011df49e76da2eed2d9c62abd7d8d19
MostafaGamalSayed <mostafagamal99418@gmail.com>
2025-02-07 11:35:56 +0200

    Add symbols for adding and removing layers in constants
    Introduce `addLayerSymbol` and `removeLayerSymbol` to manage layer operations in the shared constants. These new symbols facilitate better integration with the deck.gl layer management API.

--- packages/core/src/shared/constants.ts
+++ packages/core/src/shared/constants.ts
@@ -1,4 +1,6 @@
 import type { InjectionKey, Ref } from 'vue'
-import type { Deck } from '@deck.gl/core'
+import type { Deck, Layer } from '@deck.gl/core'
 
 export const deckInstanceSymbol = Symbol('deckInstance') as InjectionKey<Ref<Deck | null>>
+export const addLayerSymbol = Symbol('addLayer') as InjectionKey<(layer: Layer) => void>
+export const removeLayerSymbol = Symbol('removeLayer') as InjectionKey<(layer: Layer) => void>
