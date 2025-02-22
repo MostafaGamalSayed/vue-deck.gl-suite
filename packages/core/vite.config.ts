de11f7c8d43161a0a7c09878cf383a502582f6e1
MostafaGamalSayed <mostafagamal99418@gmail.com>
2025-02-22 17:39:49 +0200

    Add TypeScript declaration bundling and adjust build config
    Added `vite-plugin-dts` to generate TypeScript declarations with specific options like `outDir` and `rollupTypes`. Also updated the build configuration to disable sourcemaps and set library export mode to "named". These changes enhance type definition generation and ensure a cleaner build output.

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -4,11 +4,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { resolve } from 'path'
+import dtsPlugin from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    dtsPlugin({
+      entryRoot: './src',
+      insertTypesEntry: true, // Ensure types are included in "exports" in package.json
+      tsconfigPath: './tsconfig.app.json', // Point explicitly to the correct tsconfig
+      rollupTypes: true,      // Use rollup to bundle declaration files
+      outDir: 'dist',      // Place all declarations in the "dist" folder
+    }),
     vueDevTools(),
   ],
   resolve: {
@@ -17,6 +25,7 @@ export default defineConfig({
     },
   },
   build: {
+    sourcemap: false,
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
       // Simplified library global name
@@ -31,6 +40,7 @@ export default defineConfig({
         '@deck.gl/layers'
       ],
       output: {
+        exports: "named",
         globals: {
           vue: 'Vue',
           '@deck.gl/core': 'deck',
