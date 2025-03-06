1ac1e36b6efb68a58969eee339407de4233aaefb
MostafaGamalSayed <mostafagamal99418@gmail.com>
2025-03-07 01:39:20 +0200

    Add utility function to compare LngLat equality
    Introduces `isLngLatEqual` for comparing two LngLat-like objects. This ensures consistent and precise longitude-latitude comparison using `maplibre-gl`.

---
+++ packages/maplibre/src/utils/isLngLatEqual.ts
@@ -0,0 +1,8 @@
+import { type LngLatLike, LngLat } from 'maplibre-gl'
+
+export function isLngLatEqual(one: LngLatLike, two: LngLatLike): boolean {
+  const firstPosition = LngLat.convert(one)
+  const secondPosition = LngLat.convert(two)
+
+  return firstPosition.lng === secondPosition.lng && firstPosition.lat === secondPosition.lat
+}
