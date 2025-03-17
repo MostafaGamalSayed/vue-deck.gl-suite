90b826689297fbf690eb043f713646e766799571
MostafaGamalSayed <mostafagamal99418@gmail.com>
2025-03-17 03:45:24 +0200

    Refactor router and set up Maplibre package structure
    Replaced and expanded the router definitions, introducing new routes for various layers and maps. Removed unused Vue plugin setup and added initial configurations for the Maplibre package, including git attributes, editor settings, and Prettier rules. Added a basic `index.html` for the Maplibre package and a `.gitignore` file.

---
+++ packages/playground/src/router/index.js
@@ -0,0 +1,79 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/arc-layer',
+      name: 'arc-layer',
+      component: () => import('../views/ArcLayer.vue'),
+    },
+    {
+      path: '/point-cloud-layer',
+      name: 'point-cloud-layer',
+      component: () => import('../views/PointCloudLayer.vue'),
+    },
+    {
+      path: '/column-layer',
+      name: 'column-layer',
+      component: () => import('../views/ColumnLayer.vue'),
+    },
+    {
+      path: '/maplibre-map',
+      name: 'maplibre-map',
+      component: () => import('../views/MaplibreMap.vue'),
+    },
+    {
+      path: '/maplibre/tiles3d-layer',
+      name: 'maplibre-tiles3d-layer',
+      component: () => import('../views/maplibre/Tile3dLayer.vue'),
+    },
+    {
+      path: '/maplibre/trips-layer',
+      name: 'maplibre-trips-layer',
+      component: () => import('../views/maplibre/TripsLayer.vue'),
+    },
+    {
+      path: '/maplibre/path-layer',
+      name: 'maplibre-path-layer',
+      component: () => import('../views/maplibre/PathLayer.vue'),
+    },
+    {
+      path: '/maplibre/wms-layer',
+      name: 'maplibre-wms-layer',
+      component: () => import('../views/maplibre/WMSLayer.vue'),
+    },
+    {
+      path: '/maplibre/heatmap-layer',
+      name: 'maplibre-heatmap-layer',
+      component: () => import('../views/maplibre/HeatmapLayer.vue'),
+    },
+    {
+      path: '/maplibre/geojson-layer',
+      name: 'maplibre-geojson-layer',
+      component: () => import('../views/maplibre/GeojsonLayer.vue'),
+    },
+    {
+      path: '/maplibre/hexagon-layer',
+      name: 'maplibre-hexagon-layer',
+      component: () => import('../views/maplibre/HexagonLayer.vue'),
+    },
+    {
+      path: '/maplibre/icon-layer',
+      name: 'maplibre-icon-layer',
+      component: () => import('../views/maplibre/IconLayer.vue'),
+    },
+    {
+      path: '/maplibre/arc-layer',
+      name: 'maplibre-arc-layer',
+      component: () => import('../views/maplibre/ArcLayer.vue'),
+    },
+    {
+      path: '/maplibre/grid-layer',
+      name: 'maplibre-grid-layer',
+      component: () => import('../views/maplibre/GridLayer.vue'),
+    },
+  ],
+})
+
+export default router
