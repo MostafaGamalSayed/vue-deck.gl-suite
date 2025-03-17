049f25c3955b36737a754a9b48225cda833d2ea7
MostafaGamalSayed <mostafagamal99418@gmail.com>
2025-03-17 03:42:39 +0200

    Add TripsLayer import and adjust Map component structure
    Introduced the `TripsLayer` import in the script section for future use. Adjusted the structure of the `Map` and `PathLayer` components inside the template for better organization and code readability. This change prepares for potential feature enhancements and ensures cleaner markup.

--- packages/playground/src/views/maplibre/PathLayer.vue
+++ packages/playground/src/views/maplibre/PathLayer.vue
@@ -1,27 +1,29 @@
 <script setup>
 import { DeckGL, Map } from '@vue-deckgl-suite/maplibre'
 import { PathLayer } from '@vue-deckgl-suite/layers'
+import { TripsLayer } from '@vue-deckgl-suite/layers'
 
 const style = 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json'
 </script>
 
 <template>
   <DeckGL :get-tooltip="({ object }) => object && object.name">
-    <Map height="100vh" :style :center="[-122.4, 37.74]" :zoom="11" />
-    <PathLayer
-      id="PathLayer"
-      data="https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/bart-lines.json"
-      :getColor="
+    <Map height="100vh" :style :center="[-122.4, 37.74]" :zoom="11">
+      <PathLayer
+        id="PathLayer"
+        data="https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/bart-lines.json"
+        :getColor="
         (d) => {
           const hex = d.color
           // convert to RGB
           return hex.match(/[0-9a-f]{2}/g).map((x) => parseInt(x, 16))
         }
       "
-      :getPath="(d) => d.path"
-      :getWidth="100"
-      :pickable="true"
-    />
+        :getPath="(d) => d.path"
+        :getWidth="100"
+        :pickable="true"
+      />
+    </Map>
   </DeckGL>
 </template>
 
