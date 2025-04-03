920792ef2c1f68476c79fdd9abd2701fd6800cca
MostafaGamalSayed <mostafagamal99418@gmail.com>
2025-04-03 14:49:41 +0200

    Add BetaHeatmapLayer.vue for heatmap visualization
    Introduce a new Vue component to render a heatmap layer using MapLibre, Deck.gl, and a data source for San Francisco bike parking. This component demonstrates map styling, tooltips, and data aggregation features.

---
+++ packages/playground/src/views/maplibre/BetaHeatmapLayer.vue
@@ -0,0 +1,33 @@
+<script setup>
+import {  Map, BetaDeckGL } from '@vue-deckgl-suite/maplibre'
+import { HeatmapLayer } from '@vue-deckgl-suite/layers'
+
+const style = 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json'
+
+</script>
+
+<template>
+  <BetaDeckGL :get-tooltip="({ object }) => object && object.name">
+    <Map
+      height="100vh"
+      :style
+      :center="[-122.4, 37.74]"
+      :zoom="11"
+      :max-zoom="20"
+      :pitch="30"
+      :bearing="0"
+    />
+    <HeatmapLayer
+      id="HeatmapLayer"
+      :data="'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/sf-bike-parking.json'"
+      :aggregation="'SUM'"
+      :getPosition="d => d.COORDINATES"
+      :getWeight="d => d.SPACES"
+      :radiusPixels="25"
+    />
+  </BetaDeckGL>
+</template>
+
+<style lang="scss">
+@import 'maplibre-gl/dist/maplibre-gl.css';
+</style>
