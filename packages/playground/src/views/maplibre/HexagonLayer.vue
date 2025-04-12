d4612d32386aa146d551e5b68edc105029678a4e
MostafaGamalSayed <mostafagamal99418@gmail.com>
2025-04-12 22:05:15 +0200

    Refactor HexagonLayer to use reactive state and async data.
    Converted static variables to reactive refs for dynamic updates. Added an onMounted lifecycle hook to fetch data asynchronously and update layer properties such as color range and radius after a delay. This improves flexibility and enables dynamic data handling for the layer.

--- packages/playground/src/views/maplibre/HexagonLayer.vue
+++ packages/playground/src/views/maplibre/HexagonLayer.vue
@@ -1,17 +1,49 @@
 <script setup>
 import { DeckGL, Map } from '@vue-deckgl-suite/maplibre'
 import { HexagonLayer } from '@vue-deckgl-suite/layers'
+import { onMounted, ref } from 'vue'
 
-const style = 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json'
-
-const colorRange = [
+const data = ref([])
+const colorRange = ref([
   [1, 152, 189],
   [73, 227, 206],
   [216, 254, 181],
   [254, 237, 177],
   [254, 173, 84],
   [209, 55, 78],
-]
+])
+const radius = ref(200)
+
+const style = 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json'
+
+
+function getData() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/sf-bike-parking.json')
+    }, 1000)
+  })
+}
+
+onMounted(async () => {
+  data.value = await getData()
+  setTimeout(() => {
+    colorRange.value = [
+      [1, 152, 189],
+      [73, 227, 206],
+      [216, 254, 181],
+      [254, 237, 177],
+      [254, 173, 84],
+      [209, 55, 78],
+      [166, 86, 40],
+      [247, 129, 191],
+      [153, 153, 153],
+    ]
+
+    radius.value = 100
+  }, 5000)
+})
+
 </script>
 
 <template>
@@ -27,14 +59,14 @@ const colorRange = [
     />
     <HexagonLayer
       id="HexagonLayer"
-      data="https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/sf-bike-parking.json"
+      :data="`https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/sf-bike-parking.json`"
       :extruded="true"
       :color-range="colorRange"
       :get-position="(d) => d.COORDINATES"
       :get-color-weight="(d) => d.SPACES"
       :get-elevation-weight="(d) => d.SPACES"
       :elevation-scale="4"
-      :radius="200"
+      :radius
       :pickable="true"
     />
   </DeckGL>
