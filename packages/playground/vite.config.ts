c2c5e3bbe93f577e5e6536bd1a8c271657dbed3b
MostafaGamalSayed <mostafagamal99418@gmail.com>
2025-04-15 15:57:34 +0200

    Add environment variable support to Vite config
    Refactored the Vite configuration to load environment variables using `loadEnv`. Introduced a global `__GOOGLE_MAPS_KEY__` definition for better integration with external services. This enhances flexibility for environment-specific settings.

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -1,18 +1,25 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig, type PluginOption } from 'vite'
+import { defineConfig, loadEnv, type PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue() as PluginOption,
-    vueDevTools() as PluginOption,
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    define: {
+      __GOOGLE_MAPS_KEY__: JSON.stringify(env),
     },
-  },
+    plugins: [
+      vue() as PluginOption,
+      vueDevTools() as PluginOption,
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+    },
+  }
 })
