deae52d4d5f0c17c012e78e056ad1682921b0297
MostafaGamalSayed <mostafagamal99418@gmail.com>
2025-04-24 17:11:58 +0200

    Refactor ContourLayer.vue for consistent code style
    Updated formatting and syntax for improved readability and consistency. Ensured proper spacing, indentation, and alignment in template and JavaScript expressions. No functional changes were made.

--- packages/playground/src/views/maplibre/ContourLayer.vue
+++ packages/playground/src/views/maplibre/ContourLayer.vue
@@ -6,7 +6,7 @@ const style = 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json'
 </script>
 
 <template>
-  <DeckGL :getTooltip="({object}) => object && `threshold: ${object.contour.threshold}`">
+  <DeckGL :getTooltip="({ object }) => object && `threshold: ${object.contour.threshold}`">
     <Map
       height="100vh"
       :style
@@ -14,20 +14,19 @@ const style = 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json'
       :zoom="11"
       :max-zoom="20"
       :pitch="30"
-      :bearing="0"
-    />
+      :bearing="0" />
     <ContourLayer
       id="ContourLayer"
       :data="'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/sf-bike-parking.json'"
       :cellSize="200"
       :contours="[
-        {threshold: 1, color: [255, 0, 0], strokeWidth: 2, zIndex: 1},
-        {threshold: [3, 10], color: [55, 0, 55], zIndex: 0},
-        {threshold: 5, color: [0, 255, 0], strokeWidth: 6, zIndex: 2},
-        {threshold: 15, color: [0, 0, 255], strokeWidth: 4, zIndex: 3}
+        { threshold: 1, color: [255, 0, 0], strokeWidth: 2, zIndex: 1 },
+        { threshold: [3, 10], color: [55, 0, 55], zIndex: 0 },
+        { threshold: 5, color: [0, 255, 0], strokeWidth: 6, zIndex: 2 },
+        { threshold: 15, color: [0, 0, 255], strokeWidth: 4, zIndex: 3 },
       ]"
-      :getPosition="d => d.COORDINATES"
-      :getWeight="d => d.SPACES"
+      :getPosition="(d) => d.COORDINATES"
+      :getWeight="(d) => d.SPACES"
       :pickable="true"
-    /></DeckGL>
+  /></DeckGL>
 </template>
